1ea547f4d1979cc405c9ace4aa5f265efedfcd7e
Josh Bedo <joshbedo@yahoo.com>
2017-01-30 06:21:26 -0500

    Support React 0.15+ / ES2015 / Add mandatory API_KEY / Use little-loader

--- example/src/example.js
+++ example/src/example.js
@@ -1,6 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var SelectGooglePlaces = require('react-select-google-places');
+var SelectGooglePlaces = require('react-google-places');
+
+// const API_KEY = '<YOUR-API-KEY>';
+const API_KEY = 'AIzaSyC3yCuJtptjR5ToKEdsPqHvPnlQXcLMTRk';
 
 var App = React.createClass({
   onChange (value) {
@@ -10,9 +13,9 @@ var App = React.createClass({
 		return (
 			<div>
         <h2>Demo Single selection</h2>
-        <SelectGooglePlaces optionsForSelect={{multi: false, cache: false}} initialValue={{name: 'New York'}} onChange={this.onChange} />
+        <SelectGooglePlaces apiKey={API_KEY} optionsForSelect={{multi: false, cache: false}} initialValue={{label: 'New York', value: 'New York'}} onChange={this.onChange} />
         <h2>Demo Multiple selection</h2>
-        <SelectGooglePlaces optionsForSelect={{multi: true, cache: false }} initialValue={[{name: 'New York'}, {name: 'Chicago'}]} onChange={this.onChange} />
+        <SelectGooglePlaces apiKey={API_KEY} optionsForSelect={{multi: true, cache: false }} initialValue={[{label: 'New York', value: 'New York'}, {label: 'Chicago', value: 'Chicago'}]} onChange={this.onChange} />
 			</div>
 		);
 	}
